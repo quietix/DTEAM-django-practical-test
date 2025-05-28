da83fe70bb6c6576585fc9e0947d9f7b64194fe1
ivan <i.o.zashchyk@gmail.com>
2025-05-28 10:52:22 +0300

    feat(task1): add cv-list and cv-detail views

---
+++ src/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from main.views import CVListView, CVDetailView
+
+
+app_name = "main"
+
+urlpatterns = [
+    path("", CVListView.as_view(), name="cv-list"),
+    path("cv/<int:pk>/", CVDetailView.as_view(), name="cv-detail"),
+]
--- src/main/views.py
+++ src/main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from main.models import CV
 
-# Create your views here.
+
+class CVListView(ListView):
+    model = CV
+    template_name = "main/cv_list.html"
+    context_object_name = "cvs"
+    ordering = ["lastname", "firstname"]
+
+
+class CVDetailView(DetailView):
+    model = CV
+    template_name = "main/cv_detail.html"
+    context_object_name = "cv"
